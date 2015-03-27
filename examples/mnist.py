f90a804853aeebab8a67e97ce5b29a7aa439e235
Diogo Moitinho de Almeida <diogo149@gmail.com>
2015-03-26 18:42:47 -0700

    add seaborn, and another strategy

--- examples/mnist.py
+++ examples/mnist.py
@@ -69,6 +69,27 @@ def least_certain(X_train, train_idxs, y, predict_fn, objective_fn):
     assert best_idx is not None
     return best_idx
 
+# HACK to compute candidates once
+GLOBAL_CANDIDATES = []
+
+
+def least_certain_computed_once(X_train,
+                                train_idxs,
+                                y,
+                                predict_fn,
+                                objective_fn):
+    if not GLOBAL_CANDIDATES:
+        X_tmp = X_train[train_idxs]
+        preds = predict_fn(X_tmp, y, X_train)
+        idxs = set(train_idxs)
+        for idx, pred in enumerate(preds):
+            if idx not in idxs:
+                certainty = abs(pred - 0.5)
+                GLOBAL_CANDIDATES.append((idx, certainty))
+        GLOBAL_CANDIDATES.sort(key=lambda x: x[1], reverse=True)
+    idx, certainty = GLOBAL_CANDIDATES.pop()
+    return idx
+
 
 def most_variance(X_train, train_idxs, y, predict_fn, objective_fn):
     X_tmp = X_train[train_idxs]
@@ -97,36 +118,38 @@ if __name__ == "__main__":
     X = X_raw[idxs]
     y = (y_raw[idxs] == mnist_class1) + 0.0
     clf = sklearn.linear_model.LogisticRegression()
+    kwargs = dict(
+        X=X,
+        y=y,
+        num_initial_samples=100,
+        num_final_samples=200,
+        predict_fn=functools.partial(predict_model, clf),
+        test_size=0.5,
+        seed=51,
+        objective_fn=accuracy_fn,
+    )
     scores0 = pal.simulate_sequential_binary(
-        X,
-        y,
-        predict_fn=functools.partial(predict_model, clf),
         active_learning_fn=random_next_idx,
-        objective_fn=accuracy_fn,
-        num_initial_samples=10,
-        num_final_samples=50,
-        test_size=0.5)
+        **kwargs
+    )
     scores1 = pal.simulate_sequential_binary(
-        X,
-        y,
-        predict_fn=functools.partial(predict_model, clf),
         active_learning_fn=least_certain,
-        objective_fn=accuracy_fn,
-        num_initial_samples=10,
-        num_final_samples=50,
-        test_size=0.5)
+        **kwargs
+    )
     scores2 = pal.simulate_sequential_binary(
-        X,
-        y,
-        predict_fn=functools.partial(predict_model, clf),
         active_learning_fn=most_variance,
-        objective_fn=accuracy_fn,
-        num_initial_samples=10,
-        num_final_samples=50,
-        test_size=0.5)
+        **kwargs
+    )
+    scores3 = pal.simulate_sequential_binary(
+        active_learning_fn=least_certain_computed_once,
+        **kwargs
+    )
     import pylab
+    import seaborn as sns
     p0, = pylab.plot(scores0, label="random")
     p1, = pylab.plot(scores1, label="least_certain")
     p2, = pylab.plot(scores2, label="most_variance")
-    pylab.legend(handles=[p0, p1, p2])
+    p3, = pylab.plot(scores3, label="least_certain_computed_once")
+    pylab.legend(handles=[p0, p1, p2, p3])
     pylab.savefig("mnist.png")
+    pylab.clf()
